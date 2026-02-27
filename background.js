1fc06d281b1b6914794005a660c1307c4da53d37
Simon Pillon <Simon.PILLON@apside.com>
2026-02-27 12:24:56 +0100

    feat: Firefox compatibility - importScripts, scripts manifest key, gecko settings

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-import { joursFeries, getWeekJoursFeries } from './lib/joursFeries.js';
+importScripts('./lib/joursFeries.js');
 
 const TARGET_URL = 'https://psa-fs.ent.cgi.com/psc/fsprda/EMPLOYEE/ERP/c/';
 
